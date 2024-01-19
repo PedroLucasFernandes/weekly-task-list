3456d69552927782d96f8f3893fa126999404da2
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-01-18 22:44:45 -0300

    init: first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const task = document.getElementById("input-task");
+const date = document.getElementById("date");
+const button = document.getElementById("button");
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const taskValue = task.value;
+    const dateValue = new Date(date.value + "T00:00:00");
+
+    try{
+        addTask(taskValue, dateValue);
+    }catch(error){
+        console.log(error);
+        alert("Insert a valid Date");
+    }
+
+});
+
+function addTask(taskValue, dateValue){
+    
+    if(taskValue == ""){
+        alert("Insert a Task");
+        return
+    }
+
+    const timestampDate = dateValue.getTime();
+    const weekDay = dateValue.getDay();
+    const weekIDHTML = document.getElementById(`week-${weekDay}`);
+    
+    const timeNow = Date.now();
+    const weekLater = 1000 * 60 * 60 * 24 * 7;
+    const color = timeNow + weekLater < timestampDate ? "blue" : "red";
+
+    
+    weekIDHTML.innerHTML +=
+    `<div class="task-${color}">
+        <p>${dateValue.getDate()}/${dateValue.getMonth() + 1}/${dateValue.getFullYear()}</p>
+        <p>${taskValue}</p>
+    </div>`
+};
